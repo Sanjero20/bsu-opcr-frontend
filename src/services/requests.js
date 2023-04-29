1431b103923ed1bd30c0f731cbea8231f0bb4f3e
HueHueberry <huby.new1112@gmail.com>
2023-04-30 00:25:20 +0800

    Initialized requests for pmt and head

--- src/services/requests.js
+++ src/services/requests.js
@@ -1,9 +1,13 @@
+import { paramChecker, arrayParamChecker } from "./helperFunctions";
+
 ////////////////////////////
 //  Request modification  //
 ////////////////////////////
 let apiAddr = import.meta.env.MODE == 'development' ? 'http://localhost:5050/api' : '/api';
+let routeAddr = import.meta.env.MODE == 'development' ? 'http://localhost:5050' : window.location.origin;
 
-const getRequest = async (path) => {
+// modified get request for api path
+const getAPIRequest = async (path) => {
     const response = await fetch(`${apiAddr}${path}`, {
         credentials: 'include'
     });
@@ -11,7 +15,8 @@ const getRequest = async (path) => {
     return response;
 };
 
-const postRequest = async (path, body) => {
+// modified post request for api path
+const postAPIRequest = async (path, body) => {
     const response = await fetch(`${apiAddr}${path}`, {
         method: 'POST',
         credentials: 'include',
@@ -20,4 +25,55 @@ const postRequest = async (path, body) => {
     });
 
     return response;
+};
+
+// modified post request for root path
+const postRequest = async (path, body) => {
+    const response = await fetch(`${routeAddr}${path}`, {
+        method: 'POST',
+        credentials: 'include',
+        headers: { 'Content-type': 'application/json' },
+        body: JSON.stringify(body)
+    });
+
+    return response;
+};
+
+/////////////////////////
+//  Public operations  //
+/////////////////////////
+export const login = async (credentialsJSON) => {
+    paramChecker(['username', 'password'], credentialsJSON);
+    return await postRequest('/login', credentialsJSON);
+};
+
+//////////////////////
+//  PMT operations  //
+//////////////////////
+// retrieves all the opcr registered to the pmt account
+export const retrievePmtOpcr = async () => {
+    return await getAPIRequest('/pmt/read/office');
+};
+
+// marks the department's pmt as 'calibrated'
+export const acceptOpcr = async (departmentIdJSON) => {
+    paramChecker(['departmentID'], departmentIdJSON);
+    return await postAPIRequest('/pmt/status/opcr/accept', departmentIdJSON);
+};
+
+// declines an opcr of the department/s (with limited checking)
+export const declineOpcr = async (detailedOpcrArray) => {
+    paramChecker(['departmentID', 'targets'], detailedOpcrArray);
+    arrayParamChecker(['targetID', 'successIDs'], detailedOpcrArray.targets, 'targets');
+    return await postAPIRequest('/pmt/status/opcr/decline', detailedOpcrArray);
+};
+
+///////////////////////
+//  head operations  //
+// ////////////////////
+// CREATES a new opcr (this also includes the pseudo-editing functionality)
+export const createOpcr = async (opcrDetailsJSON) => {
+    paramChecker(['opcr'], opcrDetailsJSON);
+    arrayParamChecker(['target', 'keySuccess'], opcrDetailsJSON.opcr, 'opcr');
+    return await postAPIRequest('/head/create/opcr/', opcrDetailsJSON);
 };
