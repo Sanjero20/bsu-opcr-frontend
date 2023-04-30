dfb0772afdac9253e1beaf60b6f936d12f310cf2
HueHueberry <huby.new1112@gmail.com>
2023-05-01 02:08:46 +0800

    Restructure admin interface

--- src/pages/admin/Admin.styled.jsx
+++ src/pages/admin/Admin.styled.jsx
@@ -1,10 +1,13 @@
 import styled from 'styled-components';
 
 const coloring = {
-    'bg': 'wheat',
-    'bgShadow': '#857659',
-    'btnCampus': '#3424ab',
-    'btnAccounts': '#db2348'
+    'bg': '#434343',
+    'bgShadow': '#333333',
+    'btnCampus': '#4B66F9',
+    'btnCampusActive': '#5d76fc',
+    'btnAccounts': '#FF4242',
+    'btnAccountsActive': '#ff6363',
+    'formColor': '#242424'
 };
 
 export const AbsContainer = styled.div`
@@ -14,35 +17,57 @@ export const AbsContainer = styled.div`
     width: 100%;
     top: 0;
     left: 0;
-
-    display: flex;
-    justify-content: center;
-    gap: 2rem;
+    display: grid;
     align-items: center;
+    grid-template-columns: 25% 75%;
 `;
 
-export const IntroSelection = styled.div`
+export const ButtonRef = styled.button`
     cursor: pointer;
     padding: 7rem;
-    padding-left: 10rem;
-    padding-right: 10rem;
-    border-radius: 10px;
+    width: 90%;
+    border-radius: 25px;
     box-shadow: 0px 5px 5px 0px ${coloring.bgShadow};
-    transition: 0.5s;
+`;
 
-    &:hover {
-        padding: 8rem;
-        padding-left: 10.5rem;
-        padding-right: 10.5rem;
-        box-shadow: 0px 5px 15px 5px ${coloring.bgShadow};
-        transform: translateY(-0.25rem);
+export const CampusButton = styled(ButtonRef)`
+    background-color: ${coloring.btnCampus};
+    &:active {
+        background-color: ${coloring.btnCampusActive};
+    }
+`;
+    
+export const AccountsButton = styled(ButtonRef)`
+    background-color: ${coloring.btnAccounts};
+    &:active {
+        background-color: ${coloring.btnAccountsActive};
     }
 `;
 
-export const CampusButton = styled(IntroSelection)`
-    background-color: ${coloring.btnCampus};
+export const FormHolder = styled.div`
+    background-color: ${coloring.formColor};
+    border-radius: 25px;
+    box-shadow: 0px 5px 5px 0px ${coloring.bgShadow};
+    height: 100%;
+    width: 100%;
 `;
 
-export const AccountsButton = styled(IntroSelection)`
-    background-color: ${coloring.btnAccounts};
+export const Separator = styled.div`
+    margin: 0 auto;
+    height: 95%;
+    width: 95%;
+    display: flex;
 `;
+    
+export const RightSeparator = styled(Separator)`
+    align-items: center;
+    justify-contents: center;
+`;
+    
+export const LeftSeparator = styled(Separator)`
+    row-gap: 2rem;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`;
+    
