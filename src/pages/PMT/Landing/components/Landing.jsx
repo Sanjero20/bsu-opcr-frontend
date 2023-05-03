e15b473016ab024f2df960d7bb96ecbb4fe471fb
gioshwua <reanokennethjoshua11@gmail.com>
2023-05-04 00:35:46 +0800

    mapping data

--- src/pages/PMT/Landing/components/Landing.jsx
+++ src/pages/PMT/Landing/components/Landing.jsx
@@ -3,12 +3,12 @@ import { Container } from '../../../../components/ui/Container.styled';
 import { Input } from './Styled';
 import styled from 'styled-components';
 function Landing() {
+  const arr = [{ name: 'hey' }, { name: 'kenn' }, { name: 'joshua' }];
+  const Mapped = arr.map((nam) => <Depts>{nam.name} </Depts>);
   return (
     <Container>
       <Input placeholder='Search' />
-      <div>
-        <Depts />
-      </div>
+      <div>{Mapped}</div>
     </Container>
   );
 }
