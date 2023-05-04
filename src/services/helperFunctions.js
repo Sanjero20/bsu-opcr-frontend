7110f50c5eec81768282c95a5e438170688d58e7
Sanjero20 <gelosantos72@gmail.com>
2023-05-04 14:36:03 +0800

    Adjusted response

--- src/services/helperFunctions.js
+++ src/services/helperFunctions.js
@@ -3,38 +3,37 @@
 ////////////////////////
 // helper functions that will help for json formatting
 export const paramChecker = (params, jsonObj) => {
-    const missedParams = [];
-    params.forEach(param => {
-        if (!jsonObj[param])
-            missedParams.push(param);
+  const missedParams = [];
+  params.forEach((param) => {
+    if (!jsonObj[param]) missedParams.push(param);
+  });
+
+  if (missedParams.length > 0) {
+    missedParams.forEach((p) => {
+      console.error(p);
     });
 
-    if (missedParams.length > 0) {
-        missedParams.forEach(p => {
-            console.error(p);
-        });
-
-        throw 'MissedRequestParams';
-    }
+    throw 'MissedRequestParams';
+  }
 };
 
 // this param checker checks the object values of array if these
 // object has the needed key-value pairs
-export const arrayParamChecker = (arrayParam, arrayValues, arrayKey='') => {
-    const missedParams = [];
-    for (let i = 0; i < arrayValues.length; i++) {
-        const jsonObj = arrayValues[i];
-        for (let j = 0; j < arrayParam.length; j++) {
-            if (!jsonObj[arrayParam[j]])
-                missedParams.push(`${arrayKey}:${arrayParam[j]}`);
-        }
+export const arrayParamChecker = (arrayParam, arrayValues, arrayKey = '') => {
+  const missedParams = [];
+  for (let i = 0; i < arrayValues.length; i++) {
+    const jsonObj = arrayValues[i];
+    for (let j = 0; j < arrayParam.length; j++) {
+      if (!jsonObj[arrayParam[j]])
+        missedParams.push(`${arrayKey}:${arrayParam[j]}`);
     }
+  }
 
-    if (missedParams.length > 0) {
-        missedParams.forEach(p => {
-            console.error(p);
-        });
+  if (missedParams.length > 0) {
+    missedParams.forEach((p) => {
+      console.error(p);
+    });
 
-        throw 'MissedRequestParams';
-    }
+    throw 'MissedRequestParams';
+  }
 };
--- src/services/requests.js
+++ src/services/requests.js
@@ -1,50 +1,54 @@
-import { paramChecker, arrayParamChecker } from "./helperFunctions";
+import { paramChecker, arrayParamChecker } from './helperFunctions';
 
 ////////////////////////////
 //  Request modification  //
 ////////////////////////////
-let apiAddr = import.meta.env.MODE == 'development' ? 'http://localhost:5050/api' : '/api';
-let routeAddr = import.meta.env.MODE == 'development' ? 'http://localhost:5050' : window.location.origin;
+let apiAddr =
+  import.meta.env.MODE == 'development' ? 'http://localhost:5050/api' : '/api';
+let routeAddr =
+  import.meta.env.MODE == 'development'
+    ? 'http://localhost:5050'
+    : window.location.origin;
 
 // modified get request for api path
 const getAPIRequest = async (path) => {
-    const response = await fetch(`${apiAddr}${path}`, {
-        credentials: 'include'
-    });
+  const response = await fetch(`${apiAddr}${path}`, {
+    credentials: 'include',
+  });
 
-    return response;
+  return response.json();
 };
 
 // modified post request for api path
 const postAPIRequest = async (path, body) => {
-    const response = await fetch(`${apiAddr}${path}`, {
-        method: 'POST',
-        credentials: 'include',
-        headers: { 'Content-type': 'application/json' },
-        body: JSON.stringify(body)
-    });
+  const response = await fetch(`${apiAddr}${path}`, {
+    method: 'POST',
+    credentials: 'include',
+    headers: { 'Content-type': 'application/json' },
+    body: JSON.stringify(body),
+  });
 
-    return response;
+  return response.json();
 };
 
 // modified post request for root path
 const postRequest = async (path, body) => {
-    const response = await fetch(`${routeAddr}${path}`, {
-        method: 'POST',
-        credentials: 'include',
-        headers: { 'Content-type': 'application/json' },
-        body: JSON.stringify(body)
-    });
+  const response = await fetch(`${routeAddr}${path}`, {
+    method: 'POST',
+    credentials: 'include',
+    headers: { 'Content-type': 'application/json' },
+    body: JSON.stringify(body),
+  });
 
-    return response;
+  return response.json();
 };
 
 /////////////////////////
 //  Public operations  //
 /////////////////////////
 export const login = async (credentialsJSON) => {
-    paramChecker(['username', 'password'], credentialsJSON);
-    return await postRequest('/login', credentialsJSON);
+  paramChecker(['username', 'password'], credentialsJSON);
+  return await postRequest('/login', credentialsJSON);
 };
 
 //////////////////////
@@ -52,20 +56,24 @@ export const login = async (credentialsJSON) => {
 //////////////////////
 // retrieves all the opcr registered to the pmt account
 export const retrievePmtOpcr = async () => {
-    return await getAPIRequest('/pmt/read/office');
+  return await getAPIRequest('/pmt/read/office');
 };
 
 // marks the department's pmt as 'calibrated'
 export const acceptOpcr = async (departmentIdJSON) => {
-    paramChecker(['departmentID'], departmentIdJSON);
-    return await postAPIRequest('/pmt/status/opcr/accept', departmentIdJSON);
+  paramChecker(['departmentID'], departmentIdJSON);
+  return await postAPIRequest('/pmt/status/opcr/accept', departmentIdJSON);
 };
 
 // declines an opcr of the department/s (with limited checking)
 export const declineOpcr = async (detailedOpcrArray) => {
-    paramChecker(['departmentID', 'targets'], detailedOpcrArray);
-    arrayParamChecker(['targetID', 'successIDs'], detailedOpcrArray.targets, 'targets');
-    return await postAPIRequest('/pmt/status/opcr/decline', detailedOpcrArray);
+  paramChecker(['departmentID', 'targets'], detailedOpcrArray);
+  arrayParamChecker(
+    ['targetID', 'successIDs'],
+    detailedOpcrArray.targets,
+    'targets'
+  );
+  return await postAPIRequest('/pmt/status/opcr/decline', detailedOpcrArray);
 };
 
 ///////////////////////
@@ -73,7 +81,7 @@ export const declineOpcr = async (detailedOpcrArray) => {
 // ////////////////////
 // CREATES a new opcr (this also includes the pseudo-editing functionality)
 export const createOpcr = async (opcrDetailsJSON) => {
-    paramChecker(['opcr'], opcrDetailsJSON);
-    arrayParamChecker(['target', 'keySuccess'], opcrDetailsJSON.opcr, 'opcr');
-    return await postAPIRequest('/head/create/opcr/', opcrDetailsJSON);
+  paramChecker(['opcr'], opcrDetailsJSON);
+  arrayParamChecker(['target', 'keySuccess'], opcrDetailsJSON.opcr, 'opcr');
+  return await postAPIRequest('/head/create/opcr/', opcrDetailsJSON);
 };
