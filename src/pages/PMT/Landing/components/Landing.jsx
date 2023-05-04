db28413fad46f2fee65b8416c9cd2e6d8080ff57
gioshwua <reanokennethjoshua11@gmail.com>
2023-05-04 12:35:19 +0800

    designed initial fetched department/campus data

--- src/pages/PMT/Landing/components/Landing.jsx
+++ src/pages/PMT/Landing/components/Landing.jsx
@@ -1,23 +1,30 @@
 import React from 'react';
-import { Container } from '../../../../components/ui/Container.styled';
-import { Input } from './Styled';
+import { Container } from './Styled';
+import { Input, Depts, Divider } from './Styled';
 import styled from 'styled-components';
 function Landing() {
-  const arr = [{ name: 'hey' }, { name: 'kenn' }, { name: 'joshua' }];
-  const Mapped = arr.map((nam) => <Depts>{nam.name} </Depts>);
+  const arr = [
+    { name: 'hey' },
+    { name: 'kenn' },
+    { name: 'joshua' },
+    { name: 'joshua' },
+    { name: 'joshua' },
+    { name: 'joshua' },
+    { name: 'kenn' },
+    { name: 'joshua' },
+    { name: 'joshua' },
+    { name: 'joshua' },
+    { name: 'joshua' },
+  ];
+  const Mapped = arr.map((nam) => (
+    <Depts onClick={() => console.log('hehe')}>{nam.name} </Depts>
+  ));
   return (
     <Container>
       <Input placeholder='Search' />
-      <div>{Mapped}</div>
+      <Divider>{Mapped}</Divider>
     </Container>
   );
 }
 
 export default Landing;
-
-const Depts = styled.div`
-  height: 180px;
-  width: 250px;
-  border-radius: 20px;
-  background-color: grey;
-`;
