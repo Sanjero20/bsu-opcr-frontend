737ec24485fab4662a56f4084baf0b17f2da44dc
Sanjero20 <gelosantos72@gmail.com>
2023-05-04 23:35:15 +0800

    disable react-hooks/exhaustive-deps

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -11,6 +11,7 @@ module.exports = {
   settings: { react: { version: '18.2' } },
   plugins: ['react-refresh'],
   rules: {
+    'react-hooks/exhaustive-deps': 'off',
     'react-refresh/only-export-components': 'warn',
   },
 };
