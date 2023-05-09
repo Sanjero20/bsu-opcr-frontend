94dd5a197bedeb5e23c34d345d054259ccce1fb8
gioshwua <reanokennethjoshua11@gmail.com>
2023-05-10 02:21:29 +0800

    added blocks auto change color every render

--- src/pages/PMT/Landing/components/Landing.jsx
+++ src/pages/PMT/Landing/components/Landing.jsx
@@ -1,4 +1,4 @@
-import { useContext } from 'react';
+import { useContext, useState, useEffect } from 'react';
 
 import { Container } from './Styled';
 import { Input, Depts, Divider } from './Styled';
@@ -6,6 +6,25 @@ import { Input, Depts, Divider } from './Styled';
 import { DepartmentContext } from '../../index';
 
 function Landing() {
+  const [searchText, setSearchText] = useState('');
+  const [color, setColor] = useState('');
+
+  function generateRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+
+  useEffect(() => {
+    setColor(generateRandomColor());
+  }, []);
+
+  const handleSearch = (e) => {
+    setSearchText(e.target.value);
+  };
   const { department, changeMode, changeDept } = useContext(DepartmentContext);
 
   const selectDepartment = (id) => {
@@ -21,7 +40,11 @@ function Landing() {
       <Divider>
         {department.length != 0 &&
           department.map((nam) => (
-            <Depts key={nam._id} onClick={() => selectDepartment(nam._id)}>
+            <Depts
+              style={{ backgroundColor: color }}
+              key={nam._id}
+              onClick={() => selectDepartment(nam._id)}
+            >
               {nam.name}
             </Depts>
           ))}
