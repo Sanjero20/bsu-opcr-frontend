ba50efe78f06fdfa5e9137f23f7b466cbc2556c0
Sanjero20 <gelosantos72@gmail.com>
2023-05-12 22:38:19 +0800

    Add toast container and functions

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
 import { RouterProvider } from 'react-router-dom';
 import routes from './routes';
 
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.min.css';
+
+const closeInMilliseconds = 5000;
+
 function App() {
-  return <RouterProvider router={routes} />;
+  return (
+    <>
+      <RouterProvider router={routes} />;
+      <ToastContainer autoClose={closeInMilliseconds} draggable="false" />
+    </>
+  );
 }
 
 export default App;
